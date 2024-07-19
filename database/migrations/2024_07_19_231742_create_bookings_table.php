b748c925dca3f567b70098419991b36ac7482594
anamboijohn <anamboijohn@gmail.com>
2024-07-19 23:21:40 +0000

    define bookings schema

--- database/migrations/2024_07_19_231742_create_bookings_table.php
+++ database/migrations/2024_07_19_231742_create_bookings_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->dateTime('date');
             $table->timestamps();
         });
     }
