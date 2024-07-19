49efc1d2900b8fcd6e6daf9cb72e0f652db53edb
anamboijohn <anamboijohn@gmail.com>
2024-07-19 23:04:53 +0000

    Add unique name and slug fields to RolesFactory

--- database/factories/RoleFactory.php
+++ database/factories/RoleFactory.php
@@ -17,7 +17,8 @@ class RoleFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->unique()->word,
+            'slug' => $this->faker->unique()->slug,
         ];
     }
 }
