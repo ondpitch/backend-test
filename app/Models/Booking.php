b3bb429fa8ed1fef46b7bb8d2aac89504fe1f206
anamboijohn <anamboijohn@gmail.com>
2024-07-20 20:35:47 +0000

    define scope filter

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -9,6 +10,34 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Booking extends Model
 {
     use HasFactory;
+    private $startDate = false;
+    private $endDate = false;
+
+    //scope filters
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? null, function ($query, $search) {
+            $query->where(function ($query) use ($search) {
+                $query->where('title', 'like', '%' . $search . '%')
+                    ->orWhere('date', 'like', '%' . $search . '%');
+            });
+        });
+
+        if (isset($filters['start']) && isset($filters['end'])) {
+            $this->startDate = Carbon::createFromFormat('Y-m-d', $filters['start'])->startOfDay();
+            $this->endDate = Carbon::createFromFormat('Y-m-d', $filters['end'])->endOfDay();
+        }
+        $query
+            ->when(
+                $this->startDate && $this->endDate,
+                fn ($query) =>
+                $query->where(
+                    fn ($query) =>
+                    $query->whereBetween('date', [$this->startDate, $this->endDate])
+                )
+
+            );
+    }
     protected $guarded = [];
 
     // define relationship with User model
