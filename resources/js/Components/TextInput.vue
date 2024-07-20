be9a8d66fc9aacaed27a1204485ed58bb7d8d6c7
anamboijohn <anamboijohn@gmail.com>
2024-07-20 18:36:36 +0000

    refactor

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -6,18 +6,18 @@ const model = defineModel<string>({ required: true });
 const input = ref<HTMLInputElement | null>(null);
 
 onMounted(() => {
-    if (input.value?.hasAttribute('autofocus')) {
-        input.value?.focus();
-    }
+  if (input.value?.hasAttribute('autofocus')) {
+    input.value?.focus();
+  }
 });
 
 defineExpose({ focus: () => input.value?.focus() });
 </script>
 
 <template>
-    <input
-        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-        v-model="model"
-        ref="input"
-    />
+  <input
+    class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+    v-model="model"
+    ref="input"
+  />
 </template>
