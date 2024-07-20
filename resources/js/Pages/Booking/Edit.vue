ac982b58b0ec76acaeaef0f527c7c56e70f5d038
anamboijohn <anamboijohn@gmail.com>
2024-07-20 18:35:07 +0000

    design edit form

---
+++ resources/js/Pages/Booking/Edit.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+import { Head, Link, useForm } from '@inertiajs/vue3';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+
+const props = defineProps<{
+  booking: {
+    id: number;
+    title: string;
+    date: string;
+  };
+}>();
+
+const form = useForm({
+  title: props.booking.title,
+  date: props.booking.date,
+});
+
+const submit = () => {
+  // dynamicaly post to update or store rout
+  form.patch(route('bookings.update', props.booking.id), {
+    onSuccess: () => {
+      form.reset();
+    },
+  });
+};
+</script>
+<template>
+  <Head title="Create Booking" />
+
+  <AuthenticatedLayout>
+    <div class="flex flex-col min-h-screen items-center justify-center bg-white">
+      <h1 class="text-center mb-5 font-bold text-xl uppercase">book</h1>
+      <div
+        class="grid grid-cols-1 gap-3 items-center mx-auto max-w-md"
+        style="width: 80vw"
+      >
+        <form @submit.prevent="submit">
+          <div>
+            <InputLabel for="title" value="Title" />
+
+            <TextInput
+              id="title"
+              type="text"
+              class="mt-1 block w-full"
+              v-model="form.title"
+              required
+              autofocus
+            />
+
+            <InputError class="mt-2" :message="form.errors.title" />
+          </div>
+
+          <div class="mt-4">
+            <InputLabel for="password" value="Password" />
+
+            <TextInput
+              id="date"
+              type="date"
+              class="mt-1 block w-full"
+              v-model="form.date"
+              required
+            />
+
+            <InputError class="mt-2" :message="form.errors.password" />
+          </div>
+          <PrimaryButton
+            class="ms-4 mt-4 ml-auto"
+            type="submit"
+            :class="{ 'opacity-25': form.processing }"
+            :disabled="form.processing"
+          >
+            Reschedule
+          </PrimaryButton>
+        </form>
+      </div>
+    </div>
+  </AuthenticatedLayout>
+</template>
